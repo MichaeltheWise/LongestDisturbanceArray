e1a9ff07ff7ea96ed5a7b943b8f0f542634198a0
Michael <ml145616@gmail.com>
2021-04-30 18:45:27 -0400

    Fix sliding window logic and upgrade dynamic programming logic to run faster

--- LongestDisturbanceArray.py
+++ LongestDisturbanceArray.py
@@ -21,23 +21,23 @@ def longestDisturbanceArray(input_list, num):
             start = end - 1
             sign = 0
         elif sign == -1 and input_list[end] > prev:
-            if max_length < end - start:
-                max_length = end - start
+            if max_length < end - start + 1:
+                max_length = end - start + 1
             sign = 1
         elif sign == 1 and input_list[end] < prev:
-            if max_length < end - start:
-                max_length = end - start
+            if max_length < end - start + 1:
+                max_length = end - start + 1
             sign = -1
         else:
             start = end - 1
+            if max_length < 2:
+                max_length = 2
             if input_list[end] > prev:
                 sign = 1
             else:
                 sign = -1
         prev = input_list[end]
         end += 1
-    if num > 1:
-        return max_length + 1
     return max_length
 
 
@@ -49,24 +49,24 @@ def longestDisturbanceArrayDP(input_list, num):
     :return: maximum length of the sub array
     """
     # Dynamic programming implementation
-    res = [1] * num
-    sign, prev = 0, input_list[0]
+    res = [2] * num
+    prev = input_list[0]
+    res[0] = 0
+
+    if num < 2:
+        return 1
+
+    sign = 1 if input_list[1] > prev else -1
     for i in range(1, num):
         if input_list[i] == prev:
             sign = 0
+            res[i] = 1
         elif sign == -1 and input_list[i] > prev:
             res[i] = res[i-1] + 1
             sign = 1
         elif sign == 1 and input_list[i] < prev:
             res[i] = res[i-1] + 1
             sign = -1
-        else:
-            # This needs to be defaulted to 2 since the question computes the number of points not intervals
-            res[i] = 2
-            if input_list[i] > prev:
-                sign = 1
-            else:
-                sign = -1
         prev = input_list[i]
     return max(res)
 
@@ -77,12 +77,15 @@ def main():
     print(longestDisturbanceArray([9, 4, 2, 10, 7, 8, 8, 1, 9], 9))
     print(longestDisturbanceArray([4, 8, 12, 16], 4))
     print(longestDisturbanceArray([100], 1))
+    print(longestDisturbanceArray([9, 9], 2))
+    print(longestDisturbanceArray([10, 10, 10], 3))
     print(longestDisturbanceArray(ultimate_test, 100000))
 
     print("\nDynamic programming method: ")
     print(longestDisturbanceArrayDP([9, 4, 2, 10, 7, 8, 8, 1, 9], 9))
     print(longestDisturbanceArrayDP([4, 8, 12, 16], 4))
     print(longestDisturbanceArrayDP([100], 1))
+    print(longestDisturbanceArrayDP([9, 9], 2))
     print(longestDisturbanceArrayDP(ultimate_test, 100000))
 
 
